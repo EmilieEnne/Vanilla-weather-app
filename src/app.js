f7996b39d4f082599f82e9d449599a6783f90279
EmilieEnne <emilie.enne.lykkegaard@gmail.com>
2023-04-16 12:58:57 +0200

    Added real data to air quality

--- src/app.js
+++ src/app.js
@@ -3,9 +3,13 @@ function displayTemp(response) {
   let temperatureElement = document.querySelector("#temp");
   let descriptionElemet = document.querySelector("#description");
   let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   descriptionElemet.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "3f867b81a453d2baod0689b610fta810";
